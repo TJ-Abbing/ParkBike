5bf0ee400e9984d8f8eaee698f921ef24e799430
TJ-Abbing <tjabbing@gmail.com>
2023-07-26 14:37:52 +0200

    Added; option to show/hide markers.

--- ParkBike/App.js
+++ ParkBike/App.js
@@ -1,6 +1,6 @@
 // Import necessary components from React Native and Expo libraries
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Modal, Button } from 'react-native';
 import Text from './Text.js';
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
@@ -9,8 +9,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Define the main App component
 export default function App() {
-  
-  // Define state variables for location, error message, showList and mapRegion
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [showList, setShowList] = useState(false);
@@ -18,30 +16,23 @@ export default function App() {
   const [favorites, setFavorites] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
   const [mapKey, setMapKey] = useState(0);
-
-  // Define a state variable to store the bike parking spots data
   const [bikeparkingspots, setBikeparkingspots] = useState([]);
-
-  // Define a new state variable for mapLoaded and set it to false initially
   const [mapLoaded, setMapLoaded] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showAllMarkers, setShowAllMarkers] = useState(true);
+  const [showRegularMarkers, setShowRegularMarkers] = useState(true);
+  const [showFavoriteMarkers, setShowFavoriteMarkers] = useState(true);
 
-  // Use the useEffect hook to fetch the user's location and bike parking spots data when the component mounts
   useEffect(() => {
-
-    // Define an async function to fetch the user's location
     const fetchLocation = async () => {
-      // Request permission to access the user's location
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         return;
       }
     
       try {
-        // Get the user's current position
         let location = await Location.getCurrentPositionAsync({});
-        // Update the location state variable with the user's location
         setLocation(location);
-        // Set the initial map region to center on the user's location
         setMapRegion({
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
@@ -50,12 +41,10 @@ export default function App() {
         });        
       } catch (error) {
         console.log(error);
-        // If there was an error getting the user's location, set an error message
         setErrorMsg('We’re unable to show the map because access to your location data was not granted. Please enable location services in order to use this application.');
       }
     }; 
 
-    // Call the fetchLocation function to get the user's location
     fetchLocation();
 
     // Add this code inside your existing useEffect hook
@@ -74,32 +63,25 @@ export default function App() {
     // Define an async function to fetch the bike parking spots data
     const fetchBikeparkingspots = async () => {
       try {
-        // Send a GET request to the API endpoint
         const response = await fetch('https://stud.hosted.hr.nl/1014535/parkbike/api/bikeparkingspots.json');
-        // Parse the response as JSON
         const data = await response.json();
-        // Update the bikeparkingspots state variable with the fetched data
         setBikeparkingspots(data);
       } catch (error) {
         console.log(error);
       }
     };
 
-    // Call the fetchBikeparkingspots function to fetch the bike parking spots data
     fetchBikeparkingspots();
-    
+
   }, []);
 
-  // Define a function to handle when a bike parking spot is pressed in the list
   const handleSpotPress = (spot) => {
-    // Update the map region to center on the selected bike parking spot
     setMapRegion({
       latitude: spot.latitude + Math.random() * 0.000001,
       longitude: spot.longitude + Math.random() * 0.000001,
       latitudeDelta: 0.025,
       longitudeDelta: 0.025,
     });
-    // Hide the list of bike parking spots
     setShowList(false);
   };
 
@@ -113,18 +95,13 @@ export default function App() {
     setMapKey(mapKey + 1);
   };
 
-  // Render the app UI
   return (
-
     <View style={styles.container}>
-
-      {/* If we have the user's location, render the map */}
       {location ? (
         <MapView.Animated
           key={mapKey}
           style={styles.map}
           region={mapRegion}
-
           mapType="standard"
           showsUserLocation={false}
           showsMyLocationButton={false}
@@ -134,11 +111,8 @@ export default function App() {
           showsIndoors={false}
           showsCompass={false}
           rotateEnabled={true}
-
-          // Add an onMapReady prop to your MapView.Animated component
           onMapReady={() => setMapLoaded(true)}
         >
-          {/* Render a marker for the user's current location */}
           <Marker
             coordinate={{
               latitude: location.coords.latitude,
@@ -159,56 +133,52 @@ export default function App() {
             </Callout>
           </Marker>
 
-          {/* Render markers for each bike parking spot */}
-          {bikeparkingspots.map((spot) => (
-
-            <Marker
-              key={spot.id}
-              coordinate={{
-                latitude: spot.latitude,
-                longitude: spot.longitude,
-              }}
-              title={spot.name}
-              description={`Capacity: ${spot.capacity}`}
-            >
-              <Image
-                source={
-                  favorites.includes(spot.id)
-                    ? require('./images/biycle_parking_spot_favorite.png')
-                    : require('./images/biycle_parking_spot_regular.png')
-                }
-                style={{ width: 32, height: 32, borderRadius: 8 }}
-              />
-              <Callout tooltip onPress={() => {
-                if (favorites.includes(spot.id)) {
-                  updateFavorites(favorites.filter((id) => id !== spot.id));
-                } else {
-                  updateFavorites([...favorites, spot.id]);
-                }
-              }}>
-                <View style={styles.callout}>
-                  <Text style={styles.calloutTitle}>{spot.name}</Text>
-                  <Text>Capacity: {spot.capacity}</Text>
-                  <Text>{favorites.includes(spot.id) ? 'Click to remove from favorites' : 'Click to add to favorites'}</Text>
-                </View>
-              </Callout>
-            </Marker>
-
-          ))}
+          {showAllMarkers &&
+            bikeparkingspots.map((spot) => (
+              <Marker
+                key={spot.id}
+                coordinate={{
+                  latitude: spot.latitude,
+                  longitude: spot.longitude,
+                }}
+                title={spot.name}
+                description={`Capacity: ${spot.capacity}`}
+                opacity={(showRegularMarkers && !favorites.includes(spot.id)) || (showFavoriteMarkers && favorites.includes(spot.id)) ? 1 : 0}
+              >
+                <Image
+                  source={
+                    favorites.includes(spot.id)
+                      ? require('./images/biycle_parking_spot_favorite.png')
+                      : require('./images/biycle_parking_spot_regular.png')
+                  }
+                  style={{ width: 32, height: 32, borderRadius: 8 }}
+                />
+                <Callout tooltip onPress={() => {
+                  if (favorites.includes(spot.id)) {
+                    updateFavorites(favorites.filter((id) => id !== spot.id));
+                  } else {
+                    updateFavorites([...favorites, spot.id]);
+                  }
+                }}>
+                  <View style={styles.callout}>
+                    <Text style={styles.calloutTitle}>{spot.name}</Text>
+                    <Text>Capacity: {spot.capacity}</Text>
+                    <Text>{favorites.includes(spot.id) ? 'Click to remove from favorites' : 'Click to add to favorites'}</Text>
+                  </View>
+                </Callout>
+              </Marker>
+            ))
+          }
 
         </MapView.Animated>
-
       ) : errorMsg ? (
-        // If there was an error getting the user's location, show an error message
         <View style={styles.errorContainer}>
           <Text style={styles.error}>{errorMsg}</Text>
         </View>
       ) : (
-        // Otherwise, show a loading message while we wait for the user's location
         <Text>Loading map...</Text>
       )}
 
-      {/* Conditionally render the "Show List" button based on the value of the mapLoaded state variable */}
       {mapLoaded && (
         <View style={styles.buttonContainer}>
           <TouchableOpacity
@@ -217,10 +187,15 @@ export default function App() {
           >
             <Text style={styles.buttonText}>Show List</Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setShowMenu(!showMenu)}
+          >
+            <Text style={styles.buttonText}>Show/Hide</Text>
+          </TouchableOpacity>
         </View>
       )}
 
-      {/* If showList is true, render the list of bike parking spots */}
       {showList && (
         <View style={styles.listContainer}>
           <View style={styles.switchContainer}>
@@ -239,13 +214,38 @@ export default function App() {
         </View>
       )}
 
-      <StatusBar style="auto" />
+      <Modal
+        transparent={true}
+        visible={showMenu}
+        onRequestClose={() => setShowMenu(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.menu}>
+            <Button
+              title={showAllMarkers ? "Hide All Markers" : "Show All Markers"}
+              onPress={() => setShowAllMarkers(!showAllMarkers)}
+            />
+            <Button
+              title={showRegularMarkers ? "Hide Regular Markers" : "Show Regular Markers"}
+              onPress={() => setShowRegularMarkers(!showRegularMarkers)}
+            />
+            <Button
+              title={showFavoriteMarkers ? "Hide Favorite Markers" : "Show Favorite Markers"}
+              onPress={() => setShowFavoriteMarkers(!showFavoriteMarkers)}
+            />
+            <Button
+              title="Close Menu"
+              onPress={() => setShowMenu(false)}
+            />
+          </View>
+        </View>
+      </Modal>
 
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-// Define styles for the app
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -268,19 +268,21 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 16,
     alignSelf: 'center',
+    flexDirection: 'row',
   },
   button: {
     backgroundColor: '#2196F3',
     padding: 16,
     borderRadius: 8,
+    marginHorizontal: 4,
   },
   buttonText: {
     color: 'white',
-    fontWeight: 'bold'
-  },  
-  listContainer:{
-    position:'absolute',
-    backgroundColor:'#F0F0F0',
+    fontWeight: 'bold',
+  },
+  listContainer: {
+    position: 'absolute',
+    backgroundColor: '#F0F0F0',
     paddingVertical: 80,
     paddingHorizontal: 80,
     borderRadius: 8,
@@ -296,21 +298,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  switchContainer:{
-    flexDirection:'row',
-    marginBottom:8
+  switchContainer: {
+    flexDirection: 'row',
+    marginBottom: 8,
   },
-  switch:{
-    backgroundColor:'#CCCCCC',
-    paddingVertical:4,
-    paddingHorizontal:8,
-    borderRadius:4,
-    marginRight:4
+  switch: {
+    backgroundColor: '#CCCCCC',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+    marginRight: 4,
   },
-  activeSwitch:{
-    backgroundColor:'#2196F3'
+  activeSwitch: {
+    backgroundColor: '#2196F3',
   },
-  calloutTitle:{
-    fontWeight:'bold'
-  }
+  calloutTitle: {
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  menu: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    width: 250,
+  },
 });
