dfbb14b8ce5e70afeb49a7d1744ebc785b917676
TJ-Abbing <tjabbing@gmail.com>
2023-08-18 13:58:26 +0200

    Added; seperate darkMapStyle.js file.

--- ParkBike/App.js
+++ ParkBike/App.js
@@ -7,69 +7,7 @@ import * as Location from 'expo-location';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import translate from './i18n';
 import styles from './styles';
-
-const darkMapStyle = [
-  {
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#1c1c1c', // Dark background color for the map
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#1c1c1c', // Dark color for the map labels' text stroke
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#e0e0e0', // Light color for the map labels' text
-      },
-    ],
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#757575', // Dark color for administrative areas
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#2c2c2c', // Dark color for points of interest
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#424242', // Dark color for the roads
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#0D47A1', // Dark blue color for the water
-      },
-    ],
-  },
-];
+import darkMapStyle from './darkMapStyle';
 
 export default function App() {
   const [location, setLocation] = useState(null);
