108e5888fe0715909381ae2acad1d2f465997326
TJ-Abbing <tjabbing@gmail.com>
2023-08-20 17:35:14 +0200

    Added; all of the screen setting component in a seperate file called 'SettingsScreen.js.'

--- ParkBike/App.js
+++ ParkBike/App.js
@@ -230,62 +230,23 @@ const updateFavorites = async (newFavorites) => {
                   }
                 </View>
               )}
-  
+              
               {/* Settings Screen */}
-              <Modal transparent={true} visible={showMenu} onRequestClose={() => setShowMenu(false)}>
-                <View style={styles.modalContainer}>
-                  <View style={styles.menu}>
-
-                    {/* Language selection and dark mode toggle */}
-                    <TouchableOpacity onPress={() => setSelectedLanguage('en')}>
-                      <Text style={[styles.menuItem, selectedLanguage === 'en' && styles.selectedLanguage]}>
-                        English{selectedLanguage === 'en' && '  ✔'}
-                      </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setSelectedLanguage('nl')}>
-                      <Text style={[styles.menuItem, selectedLanguage === 'nl' && styles.selectedLanguage]}>
-                        Nederlands{selectedLanguage === 'nl' && '  ✔'}
-                      </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => setSelectedLanguage('de')}>
-                      <Text style={[styles.menuItem, selectedLanguage === 'de' && styles.selectedLanguage]}>
-                        Deutsch{selectedLanguage === 'de' && '  ✔'}
-                      </Text>
-                    </TouchableOpacity>
-                    <View style={styles.darkModeContainer}>
-                      <Text style={styles.darkModeText}>{translate('darkMode', selectedLanguage)}</Text>
-                      <Switch value={darkMode} onValueChange={toggleDarkMode} />
-                    </View>
-  
-                    {/* Buttons to show/hide markers and close menu */}
-                    <Button
-                      title={translate(
-                        showAllMarkers ? 'hideAllMarkers' : 'showAllMarkers',
-                        selectedLanguage
-                      )}
-                      onPress={() => setShowAllMarkers(!showAllMarkers)}
-                    />
-                    <Button
-                      title={translate(
-                        showRegularMarkers ? 'hideRegularMarkers' : 'showRegularMarkers',
-                        selectedLanguage
-                      )}
-                      onPress={() => setShowRegularMarkers(!showRegularMarkers)}
-                    />
-                    <Button
-                      title={translate(
-                        showFavoriteMarkers ? 'hideFavoriteMarkers' : 'showFavoriteMarkers',
-                        selectedLanguage
-                      )}
-                      onPress={() => setShowFavoriteMarkers(!showFavoriteMarkers)}
-                    />
-                    <Button
-                      title={translate('closeMenu', selectedLanguage)}
-                      onPress={() => setShowMenu(false)}
-                    />
-                  </View>
-                </View>
-              </Modal>
+              <SettingsScreen
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+                showAllMarkers={showAllMarkers}
+                setShowAllMarkers={setShowAllMarkers}
+                showRegularMarkers={showRegularMarkers}
+                setShowRegularMarkers={setShowRegularMarkers}
+                showFavoriteMarkers={showFavoriteMarkers}
+                setShowFavoriteMarkers={setShowFavoriteMarkers}
+                selectedLanguage={selectedLanguage}
+                setSelectedLanguage={setSelectedLanguage}
+                translate={translate}
+                setShowMenu={setShowMenu}
+                showMenu={showMenu}
+              />
             </View>
           )}
         </Stack.Screen>
--- ParkBike/SettingsScreen.js
+++ ParkBike/SettingsScreen.js
@@ -1,15 +1,78 @@
-// This file provides the settings screen for the app
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Button, Switch, Modal } from 'react-native';
 import Text from './Text.js';
+import styles from './styles';
 
-const SettingsScreen = ({ navigation, route }) => {
-  const { selectedLanguage } = route.params;
-
+const SettingsScreen = ({
+  darkMode,
+  toggleDarkMode,
+  showAllMarkers,
+  setShowAllMarkers,
+  showRegularMarkers,
+  setShowRegularMarkers,
+  showFavoriteMarkers,
+  setShowFavoriteMarkers,
+  selectedLanguage,
+  setSelectedLanguage,
+  translate,
+  setShowMenu,
+  showMenu
+}) => {
   return (
-    <View>
-      <Text>Selected Language: {selectedLanguage}</Text>
+    <Modal transparent={true} visible={showMenu} onRequestClose={() => setShowMenu(false)}>
+    <View style={styles.modalContainer}>
+      <View style={styles.menu}>
+
+        {/* Language selection and dark mode toggle */}
+        <TouchableOpacity onPress={() => setSelectedLanguage('en')}>
+          <Text style={[styles.menuItem, selectedLanguage === 'en' && styles.selectedLanguage]}>
+            English{selectedLanguage === 'en' && '  ✔'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSelectedLanguage('nl')}>
+          <Text style={[styles.menuItem, selectedLanguage === 'nl' && styles.selectedLanguage]}>
+            Nederlands{selectedLanguage === 'nl' && '  ✔'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setSelectedLanguage('de')}>
+          <Text style={[styles.menuItem, selectedLanguage === 'de' && styles.selectedLanguage]}>
+            Deutsch{selectedLanguage === 'de' && '  ✔'}
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.darkModeContainer}>
+          <Text style={styles.darkModeText}>{translate('darkMode', selectedLanguage)}</Text>
+          <Switch value={darkMode} onValueChange={toggleDarkMode} />
+        </View>
+
+        {/* Buttons to show/hide markers and close menu */}
+        <Button
+          title={translate(
+            showAllMarkers ? 'hideAllMarkers' : 'showAllMarkers',
+            selectedLanguage
+          )}
+          onPress={() => setShowAllMarkers(!showAllMarkers)}
+        />
+        <Button
+          title={translate(
+            showRegularMarkers ? 'hideRegularMarkers' : 'showRegularMarkers',
+            selectedLanguage
+          )}
+          onPress={() => setShowRegularMarkers(!showRegularMarkers)}
+        />
+        <Button
+          title={translate(
+            showFavoriteMarkers ? 'hideFavoriteMarkers' : 'showFavoriteMarkers',
+            selectedLanguage
+          )}
+          onPress={() => setShowFavoriteMarkers(!showFavoriteMarkers)}
+        />
+        <Button
+          title={translate('closeMenu', selectedLanguage)}
+          onPress={() => setShowMenu(false)}
+        />
+      </View>
     </View>
+  </Modal>
   );
 };
 
